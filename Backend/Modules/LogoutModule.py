ed622d8ab974dac5947a869cd9953de620e668e9
cbes502034 <cbes502034@gmail.com>
2025-08-17 04:07:33 +0800

    Delete Backend/Modules directory

--- Backend/Modules/LogoutModule.py
+++
@@ -1,7 +0,0 @@
-async def Logout(request):
-    for key in ["UserName", "UserID", "RegisterID"]:
-        request.session.pop(key, None)
-        
-    return {"status": True,
-            "notify": "登出成功 !",
-            "session": "尚未登入"}
