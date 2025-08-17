786d02c7f23277ee942177ce715018504c636fb7
cbes502034 <cbes502034@gmail.com>
2025-08-17 10:36:04 +0800

    Update RedisTools.py

--- ProjectTools/RedisTools.py
+++ ProjectTools/RedisTools.py
@@ -18,7 +18,7 @@ class RedisTools(RedisBase):
     def __init__(self,URL):
         super().__init__(URL)
     def TicketLock(self,seatLockKey,userSeatIndexKey,registerID):
-        return{"seatLockKey":seatLockKey,"userSeatIndexKey":userSeatIndexKey,"registerID":registerID}
+
         try:
             lock = self.r.get(seatLockKey)
             if not lock:
@@ -95,4 +95,5 @@ class RedisTools(RedisBase):
         except Exception as e:
             return {"status":False,
                     "notify":f"TicketRestoreError ! message : {type(e)} {e}"}
+
 
